456c5c9ac25a86ba6d9c9bdf8520ec02c2e9efe4
musalys <k01041036941@gmail.com>
2016-11-27 21:57:13 +0900

    hide_my_password

--- connection.py
+++ connection.py
@@ -8,7 +8,7 @@ from sqlalchemy import create_engine
 from sqlalchemy import PrimaryKeyConstraint
 from sqlalchemy.orm import sessionmaker
 
-server = 'ec2-35-164-25-57.us-west-2.compute.amazonaws.com'
-connection_string = 'mysql+mysqldb://root:Dldbstjr89!@{}:3306/navernewscrawl'.format(server)
+server = 'your_server_host'
+connection_string = 'mysql+mysqldb://root:<your_mysql_password>@{}:3306/<your_mysql_schema_name>'.format(server)
 engine = create_engine(connection_string, pool_recycle = 3600, encoding='utf-8')
 Session = sessionmaker(bind=engine)
